b619700ff5a7a2e4b29e183e47c6ee7e8541379c
floxydio <diooktar@gmail.com>
2022-10-05 09:35:37 +0700

    .

--- src/Pages/TodoSection.js
+++ src/Pages/TodoSection.js
@@ -12,10 +12,7 @@ export default function TodoSection() {
     
     await axios.post("http://103.176.79.182:3000/product", {
       nama : name,
-      harga: price,headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-type': 'application/json',
-     }
+      harga: price
     }).then(function(res) {
       console.log(res)
       window.location.reload()
